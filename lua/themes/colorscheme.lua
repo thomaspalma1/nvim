9605fd1370b859bc540c37986644a1b4105a10f2
Thomas Palma <thomaspalma777@gmail.com>
2026-05-26 15:26:46 -0300

    [feature] add theme selection with kanagawa and tokyonight
    - add tokyonight and kanagawa themes;
    - set kanagawa as default colorscheme.

---
+++ lua/themes/colorscheme.lua
@@ -0,0 +1,9 @@
+-- Installed themes
+
+vim.pack.add({"https://github.com/folke/tokyonight.nvim"})
+vim.pack.add({"https://github.com/rebelot/kanagawa.nvim"})
+
+-- Set active theme
+
+vim.cmd[[colorscheme kanagawa]]
+-- vim.cmd[[colorscheme tokyonight]]
