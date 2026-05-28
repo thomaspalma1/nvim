b4ea8acad4ca200ac7de2161068b32aa734ce620
Thomas Palma <thomaspalma777@gmail.com>
2026-05-28 12:00:12 -0300

    [refactor] move autocmd configuration to dedicated module
    - remove TextYankPost autocmd from core settings;
    - add autocmds module to init lua.

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -27,11 +27,6 @@ vim.cmd.syntax('on')                -- Enable syntax highlighting
 -- Editor Behavior
 vim.opt.updatetime = 250            -- Faster completion and better plugin responsiveness
 vim.opt.clipboard = 'unnamedplus'   -- Use system clipboard for copy/paste
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
 
 -- Encoding
 vim.opt.encoding = 'utf-8'          -- Set file encoding to UTF-8
