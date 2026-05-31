680fbf1902be730ed3a3ec945a701d00baee7685
Thomas Palma <thomaspalma777@gmail.com>
2026-05-31 20:42:22 -0300

    [feature] add telescope fzy extension and preview support
    - add telescope fzy native extension;
    - enable colorscheme preview in telescope;
    - load fzy native extension.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,12 +4,27 @@
 
 vim.pack.add({
     {
-        src = "https://github.com/nvim-lua/plenary.nvim"
-    },
-    {
-        src = "https://github.com/nvim-telescope/telescope.nvim"
+        {
+            src = "https://github.com/nvim-lua/plenary.nvim"
+        },
+        {
+            src = "https://github.com/nvim-telescope/telescope.nvim"
+        },
+        {
+            src = "https://github.com/nvim-telescope/telescope-fzy-native.nvim",
+            build = "make"
+        }
     }
 })
 
 -- Initialize Telescope with default settings
-require("telescope").setup({})
+require("telescope").setup(
+    {
+        pickers = {
+            colorscheme = {
+                enable_preview = true
+            }
+        }
+    }
+)
+require("telescope").load_extension("fzy_native")
