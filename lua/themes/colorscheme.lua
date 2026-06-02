f3f88671a53218ecdaba006aae538c478e5624cf
Thomas Palma <thomaspalma777@gmail.com>
2026-06-02 19:39:52 -0300

    [feature] add dracula colorscheme
    - add dracula theme

--- lua/themes/colorscheme.lua
+++ lua/themes/colorscheme.lua
@@ -1,16 +1,19 @@
 -- Installed themes
 
 vim.pack.add({
-    {
-        src = "https://github.com/folke/tokyonight.nvim"
-    },
-    {
-        src = "https://github.com/rebelot/kanagawa.nvim"
-    },
-    {
-        src = "https://github.com/scottmckendry/cyberdream.nvim"
-    },
-    {
-        src = "https://github.com/Mofiqul/vscode.nvim"
-    }
+	{
+		src = "https://github.com/folke/tokyonight.nvim",
+	},
+	{
+		src = "https://github.com/rebelot/kanagawa.nvim",
+	},
+	{
+		src = "https://github.com/scottmckendry/cyberdream.nvim",
+	},
+	{
+		src = "https://github.com/Mofiqul/vscode.nvim",
+	},
+	{
+		src = "https://github.com/mofiqul/dracula.nvim",
+	},
 })
