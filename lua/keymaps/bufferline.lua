621502a37290d0e351e4cc30bb03306ba085a7b6
Thomas Palma <thomaspalma777@gmail.com>
2026-06-06 21:38:25 -0300

    [feature] add bufferline picker keymap
    - add `<Leader><Tab>` mapping in `keymaps.bufferline` to trigger `BufferLinePick`;

---
+++ lua/keymaps/bufferline.lua
@@ -0,0 +1,3 @@
+-- Trigger the buffer picker using the tab key, creating an intuitive
+-- and ergonomic shortcut that matches the visual concept of tabs.
+vim.keymap.set("n", "<Leader><Tab>", "<Cmd>BufferLinePick<CR>")
