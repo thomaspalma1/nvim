819af6d5a6cb129fa78ab0fda0ed4cdbd52b39e6
Thomas Palma <thomaspalma777@gmail.com>
2026-07-17 11:39:44 -0300

    [feature] add lsp diagnostic floating window
    - add CursorHold autocmd to display LSP diagnostics
    - add floating window configuration for vim.diagnostic.open_float

--- lua/autocmds/init.lua
+++ lua/autocmds/init.lua
@@ -12,3 +12,18 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank({ higroup = "CustomYankHighlightColor", timeout = 300 })
 	end,
 })
+
+-- Automatically displays the LSP error in a floating window when hovering the cursor
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    local opts = {
+      focusable = false,
+      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+      border = 'rounded',
+      source = 'always', -- Shows where the error comes from (e.g., pyright, pylsp, etc.)
+      prefix = ' ',
+      scope = 'cursor',
+    }
+    vim.diagnostic.open_float(nil, opts)
+  end
+})
